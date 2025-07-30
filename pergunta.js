290f06b0d74b2b22093978860501c44f1896b37b
RafaelVilabela <rafael.balcota.santos@escola.pr.gov.br>
2025-07-30 12:53:22 +0000

    save do js

---
+++ pergunta.js
@@ -0,0 +1,53 @@
+criarcartao(
+    "Conhecimentos gerais",
+    "Qual a maior palavra do mundo?",
+    "Pneumoultramicroscopicossilicovulcanoconiótico"
+)
+
+criarcartao(
+    'Conhecimentos gerais',
+    'Qual a cor do cavalo branco de napoleão?',
+    'Branco'
+)
+
+criarcartao(
+    'Conhecimentos gerais',
+    'Qual o nome do filho de Dom Pedro I?',
+    'Dom Pedro II'
+)
+
+criarcartao(
+    'Conhecimento gerais',
+    'Qual o nome do desenho que o personagem principal é um pica-pau?',
+    'Pica-pau ou Woody woodpecker em inglês '
+)
+
+criarcartao(
+    'Conhecimentos gerais',
+    'Quantas copas a seleção brasileira tem?',
+    '5'
+)
+
+criarcartao(
+    '',
+    '',
+    ''
+)
+
+criarcartao(
+    'Conhecimentos gerais',
+    'Quando o google foi criado?',
+    '4 de setembro de 1998, Menlo Park, Califórnia, EUA '
+)
+
+criarcartao(
+    '',
+    '',
+    ''
+)
+
+criarcartao(
+    'Conhecimentos gerais',
+    'Quem é o dono do facebook?',
+    'Mark Zuckerberg'
+)
